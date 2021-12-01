99b1679e0d5bda3ffb3031c0a6e5d1e3066fb114
haksa789 <haksa1008@gmail.com;git config --listgit config --global user.email haksa1008@gmail.com>
2021-12-01 12:03:32 +0900

    merge

--- src/test/java/com/pongdong/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/pongdong/book/springboot/web/HelloControllerTest.java
@@ -2,13 +2,14 @@ package com.pongdong.book.springboot.web;
 
 import com.pongdong.book.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.is;
@@ -16,7 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = HelloController.class,
         excludeFilters =
                 {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
